8aaefc055e735319e85ede4db68f3d910a9d4d57
Deependra Rawal <deependra009@yahoo.com>
2018-05-09 07:46:55 -0500

    Added NgBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, ReactiveFormsModule, 
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
